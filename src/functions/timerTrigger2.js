2a2fe41d9250e5858e61f0d76bb49fcc6b2ab64b
Mitja Antila <Mitja.Antila@edu.savonia.fi>
2023-03-29 21:46:19 +0300

    Delete that.

--- src/functions/timerTrigger2.js
+++
@@ -1,8 +0,0 @@
-const { app } = require('@azure/functions');
-
-app.timer('timerTrigger2', {
-    schedule: '0 0 * * * *',
-    handler: (myTimer, context) => {
-        context.log('Timer function processed request.');
-    }
-});
